14c4fbfeba684320886f5ee0a65e07227f121c40
Calum Bradley <cal@gmx.co.uk>
2021-08-17 20:38:10 +0100

    first commit of cakeitorleaveit

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const Datastore = require('nedb');
+const bp = require('body-parser') 
+const env = require('dotenv').config();
+ 
+const app = express();
+ 
+app.use(bp.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'))
+app.use(express.json({limit: '1mb'}));
+
+const PORT = process.env.PORT || 5000;
+ 
+app.listen(PORT), () => console.log('server running');
+
+
+
+app.post('/data', (req, res) => {
+   
+    
+    // res.send(console.log('Received POST request from CLIENT'))
+    //Database
+    const database = new Datastore('./database/orders.db')
+    database.loadDatabase()
+    database.insert(req.body);
+    console.log('Added cake order to the database');
+    
+    //console.log(req.body[0]);
+   
+    //Accessing array then the object property of price
+    let price = req.body[0].price
+    // //Logging the price
+    console.log(`Cake price is: £${price}`);
+   
+    //Price is a number
+    console.log(typeof price);  
+    
+    app.post('/create-checkout-session', async (req, res) => {
+     
+        const session = await stripe.checkout.sessions.create({
+            success_url: `http://localhost:${process.env.PORT}/success.html`,
+            cancel_url: `http://localhost:${process.env.PORT}/cancel.html`,
+            payment_method_types: ['card'],
+            mode: 'payment',
+            line_items: [
+              {
+                price_data: {
+                  currency: 'gbp',
+                  product_data: {
+                    name: 'T-shirt',
+                  },
+                  unit_amount: 2000,
+                },
+                quantity: 1,
+              },
+            ]
+        })
+        .then(console.log('Success'))
+        .catch(console.log('Fail'))
+ 
+ 
+        return res.redirect(303, session.url);
+    })
+         
+})
---
+++ public/data.js
@@ -0,0 +1,73 @@
+
+let cakeData = [];
+const addData = (ev) => {
+    ev.preventDefault()
+    //console.log('Sucessfully prevented form submission');
+    //console.log('Creating data object');
+ 
+    let cakeShape = document.getElementsByName('selectedcake');
+    for (let cake of cakeShape) {
+      if (cake.checked) {
+        //console.log(cake.checked);
+        selCakeShape = cake.value;
+        break;
+      }
+      else
+        selCakeShape = "";
+    }
+ 
+    if (getIcingSugar() ===5) {icing = true}
+    else {icing = false}
+ 
+ 
+    let data =
+    {
+        UniqueId: Date.now(),
+        cakeSize : document.getElementById('cakeSize').value,
+        cakeShape : selCakeShape,
+        sponge : document.getElementById('sponge').value,
+        butterCream: document.getElementById('butterCream').value,
+        icing: icing,
+        specialDecoration: document.querySelector("#decTextArea").value,
+        cakeMessage: document.querySelector("#cakeMessageTextArea").value,
+        dateTime: document.querySelector("#dateAndTimeReq").value,
+        price: calculateTotal()
+    }
+    //console.log(`Customers unique ID is : ${data.id}`);
+    //console.log(`Customers name is : ${data.name}`);
+    //console.log('Putting the data taken into an array');
+   
+    
+    cakeData.push(data)
+   
+    localStorage.setItem('cakeData', JSON.stringify(cakeData));
+   
+    //add back to force post the form
+    //document.querySelector('form').submit()
+ 
+    let options =
+{
+  method: 'POST',
+  headers:
+  {
+    'Content-Type': 'application/json; charset=utf-8'
+  }
+  ,
+  body: JSON.stringify(cakeData)
+}
+ 
+//Req - Represents a resource request.
+//Res - Represents the response to a request.
+ 
+ 
+fetch('/data', options)
+  .then(res => { if (res.ok) {console.log('Success')}})
+  .then(data => data);
+ 
+  //remove the data from the array as a last step
+  cakeData.shift(data);
+ 
+  document.querySelector('form').reset()
+}
+ 
+document.querySelector("#btn").addEventListener('click', addData);
